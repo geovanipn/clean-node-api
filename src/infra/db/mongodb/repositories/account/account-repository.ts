196e7c4fb2b698182bff5c158ec31dfa31a267e6
Geovani Nepomoceno <geovanipn@gmail.com>
2022-04-03 12:01:39 -0300

    feat: ensure AccountMongoRepository returns an account on success

---
+++ src/infra/db/mongodb/repositories/account/account-repository.ts
@@ -0,0 +1,13 @@
+import { AccountModel } from '../../../../../domain/models/account';
+import { AddAccountRepository } from '../../../../../data/protocols/add-account-repository';
+import { AddAccountModel } from '../../../../../domain/usecases/add-account';
+import { MongoHelper } from '../../helpers/mongo-helper';
+
+export class AccountRepository implements AddAccountRepository {
+  async add (accountData: AddAccountModel): Promise<AccountModel> {
+    const accountCollection = MongoHelper.getCollection('accounts');
+    const result = await accountCollection.insertOne(accountData);
+    const account = { id: result.insertedId.toHexString(), ...accountData };
+    return account;
+  }
+}
