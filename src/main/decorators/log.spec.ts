6a02e205c6a1ef16b887083593c7ced1a2d27c16
Geovani Nepomoceno <geovanipn@gmail.com>
2022-05-03 07:26:45 -0300

    feat: ensure LogRepository creates an error log on success

--- src/main/decorators/log.spec.ts
+++ src/main/decorators/log.spec.ts
@@ -33,7 +33,7 @@ const makeController = (): Controller => {
 
 const makeLogErrorRepository = (): LogErrorRepository => {
   class LogErrorRepositoryStub implements LogErrorRepository {
-    async log (stack: string): Promise<void> {
+    async logError (stack: string): Promise<void> {
       return await new Promise(resolve => resolve());
     }
   }
@@ -80,7 +80,7 @@ describe('Log Controller Decorator', () => {
 
   test('Should call LogErrorRepository with correct error if controller returns a server error', async () => {
     const { sut, controllerStub, logErrorRepositoryStub } = makeSut();
-    const logSpy = jest.spyOn(logErrorRepositoryStub, 'log');
+    const logSpy = jest.spyOn(logErrorRepositoryStub, 'logError');
     jest.spyOn(controllerStub, 'handle').mockReturnValueOnce(new Promise(resolve => resolve(makeServerError())));
     await sut.handle(makeFakeHttpRequest());
     expect(logSpy).toHaveBeenCalledWith('any_stack');
